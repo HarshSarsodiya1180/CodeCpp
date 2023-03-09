e3737180ac3934f0ea27561fad6ef3933c5db089
unknown <sarsodiyah@gmail.com>
2023-03-10 04:43:04 +0530

    first commit

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=4;
+    int b=6;
+    cout<<"a AND b "<<(a&b)<<endl;
+    cout<<"a OR b "<<(a|b)<<endl;
+    cout<<"a NOT "<<(~a)<<endl;
+    cout<<"a XOR b "<<(a^b)<<endl;
+   return 0; 
+} 
